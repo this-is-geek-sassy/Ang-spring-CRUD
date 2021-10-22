798f32f37188754e0a69a1f7d5995abf28d24dcf
saswata_mishra <saswata_mishra@persistent.com>
2021-10-22 16:40:01 +0530

    resolved httpclientmodule related bug

--- front_end/crudProject/src/app/app.module.ts
+++ front_end/crudProject/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClient } from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +14,7 @@ import { EmployeeListComponent } from './employee-list/employee-list.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClient
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
